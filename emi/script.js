2a9b27b43b5330b1c378523b4a5f58e50aad5071
Dinku <luciferdas1234@gmail.com>
2024-12-08 05:21:13 +0530

    calculation part corrected
    added all the input fields added the mathematical function for the emi calc. did to 4 decimal points. and it is all done in the immediate field cont.

--- emi/script.js
+++ emi/script.js
@@ -1,18 +1,59 @@
-const principalAmount = document.getElementById("principalAmount");
-const output = document.getElementById("demoCont");
-
-/*document.addEventListener("click", principalAmountFunc);
-
-function principalAmountFunc() {
- var x = document.getElementById("principalAmount").value;
- output.textContent =`this is the value ${x}`;
- console.log(x);
-}*/
-
+/*
 function updateOutput() { 
-    output.textContent = `Current input value: ${principalAmount.value}`; 
+  output.textContent = `Current input value: ${principalAmount.value}`; 
 }
 
-principalAmount.addEventListener('input', updateOutput);
+principalAmount.addEventListener('input', updateOutput);*/
 
-//principalAmountFunc();
+
+const principalAmountCont = document.getElementById("principalAmount");
+const rateOfInterestCont = document.getElementById("rateOfInterest");
+const timePeriodCont = document.getElementById("timePeriod");
+const emiCalculatedCont = document.getElementById("emiCalculated");
+
+
+const output = document.getElementById("demoCont");
+
+
+var principalAmount;
+var rateOfInterest;
+var timePeriod;
+
+function calculateEmi(){
+    var emi;
+
+    var frag1 = principalAmount* (rateOfInterest/100)/12;
+    var frag2 = Math.pow((1 + (rateOfInterest/100)/12),(timePeriod*12));
+    var frag3 = Math.pow((1 + (rateOfInterest/100)/12),(timePeriod*12));
+    var frag4 = frag3 - 1;
+
+    //limiting to 4 decimal digits
+    frag1 = Math.floor(frag1 * 10000) / 10000;
+    frag2 = Math.floor(frag2 * 10000) / 10000;
+    frag4 = Math.floor(frag4 * 10000) / 10000;
+
+    emi = ((frag1.toFixed(4)*frag2.toFixed(4))/frag4.toFixed(4)).toFixed(4);
+
+    console.log("frag 1 2 4");
+    console.log(frag1.toFixed(4));
+    //console.log(frag2);
+    //console.log(frag4);
+    //console.log(rateOfInterest);
+    //console.log(timePeriod);
+    return emi;
+}
+
+//don't touch it will break
+document.addEventListener("input", function(){
+ 
+    output.textContent = `Current input value: ${principalAmountCont.value}`;
+    principalAmount = principalAmountCont.value;
+    rateOfInterest = rateOfInterestCont.value;
+    timePeriod = timePeriodCont.value;
+    emiCalculatedCont.textContent = calculateEmi();
+    //console.log(rateOfInterest);
+    //console.log(timePeriod);
+    console.log(calculateEmi());
+  });
+
+
