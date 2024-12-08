3110349f1969cffd6d096e51b6297a20935b3607
Dinku <luciferdas1234@gmail.com>
2024-12-08 07:02:19 +0530

    added a pie chart

--- emi/script.js
+++ emi/script.js
@@ -1,28 +1,29 @@
 /*
 function updateOutput() { 
-  output.textContent = `Current input value: ${principalAmount.value}`; 
+  output.textContent = `Current input value: ${principleAmount.value}`; 
 }
 
-principalAmount.addEventListener('input', updateOutput);*/
+principleAmount.addEventListener('input', updateOutput);*/
 
 
-const principalAmountCont = document.getElementById("principalAmount");
+const principleAmountCont = document.getElementById("principleAmount");
 const rateOfInterestCont = document.getElementById("rateOfInterest");
 const timePeriodCont = document.getElementById("timePeriod");
 const emiCalculatedCont = document.getElementById("emiCalculated");
-
+const pieChartCont = document.getElementById("pieChart");
+const interestPaidCont = document.getElementById("interestPaid");
 
 const output = document.getElementById("demoCont");
 
 
-var principalAmount;
+var principleAmount;
 var rateOfInterest;
 var timePeriod;
 
 function calculateEmi(){
     var emi;
 
-    var frag1 = principalAmount* (rateOfInterest/100)/12;
+    var frag1 = principleAmount* (rateOfInterest/100)/12;
     var frag2 = Math.pow((1 + (rateOfInterest/100)/12),(timePeriod*12));
     var frag3 = Math.pow((1 + (rateOfInterest/100)/12),(timePeriod*12));
     var frag4 = frag3 - 1;
@@ -34,26 +35,63 @@ function calculateEmi(){
 
     emi = ((frag1.toFixed(4)*frag2.toFixed(4))/frag4.toFixed(4)).toFixed(4);
 
-    console.log("frag 1 2 4");
-    console.log(frag1.toFixed(4));
+    //console.log("frag 1 2 4");
+    //console.log(frag1.toFixed(4));
     //console.log(frag2);
     //console.log(frag4);
     //console.log(rateOfInterest);
     //console.log(timePeriod);
     return emi;
 }
+var interestAmount;
+//google.charts.load('current', {'packages':['corechart']});
+//google.charts.setOnLoadCallback(pieChart);
+
+
+function pieChart(){
+    var PercentInterestOfEmi = interestAmount/ (calculateEmi()* timePeriod * 12);
+    var interestAmountDeg  = (Math.floor(((PercentInterestOfEmi*360)*100)/100)).toFixed(2);
+    var principleAmountDeg = 360 - interestAmountDeg;
+  
+    /*const colors = ['red', 'yellow']; // Example colors
+    const angles = [interestAmountDeg, principleAmountDeg]; // Corresponding angles
+
+    // Create gradient dynamically
+   // const gradient = `conic-gradient(
+     // ${colors.map((color, index) => `${color} ${angles[index]}deg`).join(', ')}
+    //)`;
+    const gradient = `conic-gradient( red ${interestAmountDeg + "deg"} , yellow ${principleAmountDeg + "deg"} )`; 
+    
+    
+    pieChartCont.style.background = gradient;*/
+
+   document.documentElement.style.setProperty('--principleAmountCss', `${principleAmountDeg + "deg"}`);
+
+   document.documentElement.style.setProperty('--interestAmountCss', `${interestAmountDeg + "deg"}`);
+
+
+
+    console.log("interest amount");
+    console.log(interestAmountDeg);
+    console.log("principle amount deg");
+    console.log(principleAmountDeg);
+}
 
 //don't touch it will break
 document.addEventListener("input", function(){
  
-    output.textContent = `Current input value: ${principalAmountCont.value}`;
-    principalAmount = principalAmountCont.value;
+    output.textContent = `Current input value: ${principleAmountCont.value}`;
+    principleAmount = principleAmountCont.value;
     rateOfInterest = rateOfInterestCont.value;
     timePeriod = timePeriodCont.value;
     emiCalculatedCont.textContent = calculateEmi();
     //console.log(rateOfInterest);
     //console.log(timePeriod);
-    console.log(calculateEmi());
+   // console.log(calculateEmi());
+    interestAmount = calculateEmi() * timePeriod * 12 - principleAmount;
+    interestAmount = Math.floor((interestAmount*100)/100).toFixed(2);
+    pieChart();
+    interestPaidCont.textContent = interestAmount; 
   });
 
 
